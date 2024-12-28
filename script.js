9d33db47c7c5073c1ad89c8000358302fe479094
Tamilarasan-wd <122092595+Tamilarasan-wd@users.noreply.github.com>
2024-12-28 11:24:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // You can add code here to handle form submission, e.g., send data to a server or display a thank you message.
+    alert('Form submitted! Thank you for contacting us.');
+});
